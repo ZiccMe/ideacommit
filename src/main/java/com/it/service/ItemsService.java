3bf6fcbfb5a6701cabcdd49e5a5df6f993dfd319
zic <840037399@qq.com>
2020-08-20 15:52:00 +0800

    用idea提交的项目

---
+++ src/main/java/com/it/service/ItemsService.java
@@ -0,0 +1,13 @@
+package com.it.service;
+
+import com.it.domain.Items;
+
+/**
+ * @Author: Zic
+ * @Data: 2020-08-15 19:51
+ * Copyright©Zic 2020-2020. All rights reserved.
+ */
+
+public interface ItemsService {
+    public Items findById(Integer id);
+}
